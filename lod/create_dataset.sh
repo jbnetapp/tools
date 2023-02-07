befb9b93743c7bc6e0df9ec50b958cc5bff4509f
root <root@centos1.demo.netapp.com>
2023-02-07 09:13:15 +0000

    Add Random limis wait time by args

--- lod/create_dataset.sh
+++ lod/create_dataset.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
+RANDOM=$$
 for dir in {1..10};
   do
     mkdir -p Top_Dir_$dir/Sub_Dir_${dir}{1..10} ;
@@ -15,39 +17,40 @@ for dir in {1..10};
               cmd="dd if=src_file of=$sub_dir/test_file_$rand.$count.pdf bs=4k count=256 iflag=fullblock "
               echo "CMD - $cmd";
               $cmd 2> /dev/null
-              sleep 2;
               cmd="dd if=src_file of=$sub_dir/test_file_$rand.$count.jpg bs=4k count=256 iflag=fullblock "
               echo "CMD - $cmd";
               $cmd 2> /dev/null
-              sleep 2;
               cmd="dd if=src_file of=$sub_dir/test_file_$rand.$count.png bs=4k count=256 iflag=fullblock "
               echo "CMD - $cmd";
               $cmd 2> /dev/null
-              sleep 2;
               cmd="dd if=src_file of=$sub_dir/test_file_$rand.$count.txt bs=4k count=256 iflag=fullblock "
               echo "CMD - $cmd";
               $cmd 2> /dev/null
-              sleep 2;
               cmd="dd if=src_file of=$sub_dir/test_file_$rand.$count.html bs=4k count=256 iflag=fullblock "
               echo "CMD - $cmd";
               $cmd 2> /dev/null
-              sleep 2;
               cmd="dd if=src_file of=$sub_dir/test_file_$rand.$count.mp4 bs=4k count=256 iflag=fullblock "
               echo "CMD - $cmd";
               $cmd 2> /dev/null
-              sleep 2;
               cmd="dd if=src_file of=$sub_dir/test_file_$rand.$count.doc bs=4k count=256 iflag=fullblock "
               echo "CMD - $cmd";
               $cmd 2> /dev/null
-              sleep 2;
               cmd="dd if=src_file of=$sub_dir/test_file_$rand.$count.gif bs=4k count=256 iflag=fullblock "
               echo "CMD - $cmd";
               $cmd 2> /dev/null
-              sleep 2;
               cmd="dd if=src_file of=$sub_dir/test_file_$rand.$count.xyz bs=4k count=256 iflag=fullblock "
               echo "CMD - $cmd";
               $cmd 2> /dev/null
           done
-          echo "Press CTRL+C to stop. The dataset creation will continue after 30 seconds..";sleep 30;
+          echo "Imput random [$1] [$2]" 
+          if [ -z "$2" ] ; then
+                R=1
+          else
+		echo Random $2 
+                DIFF=$(($2-$1+1))
+                R=$(($(($RANDOM%$DIFF))+$1))
+          fi
+          echo "Press CTRL+C to stop. The dataset creation will continue after $R seconds.."
+          sleep $R;
         done
     done
