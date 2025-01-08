f79b0f017680d86402e97d485a89c3d7003d1fc4
root <root@rhel1.demo.netapp.com>
2025-01-08 14:23:31 -0500

    add sssd configuration

---
+++ lod/NFS-Kerberos/0-Create-Unix-User.ps1
@@ -0,0 +1,13 @@
+#############################################################################################
+## PowerShell Script
+## Release 0.1
+#############################################################################################
+
+New-ADGroup -Name Unix -GroupScope DomainLocal -OtherAttributes @{'gidnumber'='10001'}
+Get-ADGroup Unix -Properties gidNumber
+
+$password = ConvertTo-SecureString "Netapp1!" -AsPlainText -Force
+
+New-ADUser -SamAccountName user1 -UserPrincipalName user1@DEMO.NETAPP.COM -Name user1 -AccountPassword $password -OtherAttributes @{'uid'="user1";'uidNumber'="10001";'gidNumber'="10001";'unixHomeDirectory'='/home/user1';'loginShell'='/bin/bash'} -Enabled 1 -PasswordNeverExpires 1
+
+New-ADUser -SamAccountName user2 -UserPrincipalName user2@DEMO.NETAPP.COM -Name user2 -AccountPassword $password -OtherAttributes @{'uid'="user2";'uidNumber'="10002";'gidNumber'="10001";'unixHomeDirectory'='/home/user2';'loginShell'='/bin/bash'} -Enabled 1 -PasswordNeverExpires 1
