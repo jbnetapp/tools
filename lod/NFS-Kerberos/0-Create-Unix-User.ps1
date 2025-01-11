767ff201bed36a51f02c8505e426f44acd77618a
jbnetapp <jerome.blanchet@netapp.com>
2025-01-11 16:58:38 +0100

    add dns

--- lod/NFS-Kerberos/0-Create-Unix-User.ps1
+++ lod/NFS-Kerberos/0-Create-Unix-User.ps1
@@ -3,6 +3,23 @@
 ## Release 0.1
 #############################################################################################
 
+$configFilePath = "Setup.conf"
+$configContent = Get-Content -Path $configFilePath | Where-Object { $_ -notmatch '^\s*#' -and $_.Trim() -ne "" }
+$configHashTable = @{}
+
+foreach ($line in $configContent) {
+    # Split the line into key and value based on the '=' character
+    $key, $value = $line -split '='
+
+    # Trim any whitespace from the key and value
+    $key = $key.Trim()
+    $value = $value.Trim()
+
+    # Add the key-value pair to the hashtable
+    $configHashTable[$key] = $value
+}
+$configHashTable["SVM1"]
+$configHashTable["DOMAIN"]
 
 New-ADGroup -Name Unix -GroupScope DomainLocal -OtherAttributes @{'gidnumber'='10001'}
 Get-ADGroup Unix -Properties gidNumber
@@ -16,7 +33,11 @@ New-ADUser -SamAccountName user2 -UserPrincipalName user2@DEMO.NETAPP.COM -Name 
 New-ADUser -SamAccountName user3 -UserPrincipalName user3@DEMO.NETAPP.COM -Name user3 -AccountPassword $password -OtherAttributes @{'uid'="user3";'uidNumber'="10003";'gidNumber'="10001";'unixHomeDirectory'='/home/user3';'loginShell'='/bin/bash'} -Enabled 1 -PasswordNeverExpires 1
 
 # Add DNS Entry for Kerberos IP (Mut be validated)
-Add-DnsServerResourceRecordA -Name "NFS-SVM1" -IPv4Address "192.168.0.130" -ZoneName "demo.netapp.com" -ComputerName "DC1.demo.netapp.com"
+$NFSKerberosName="NFS-" + $configHashTable["SVM1"]
+$SVMipAddress = [System.Net.Dns]::GetHostAddresses($configHashTable["SVM1"]).IPAddressToString
+$DCname = "DC1" + $configHashTable["DOMAIN"]
+Add-DnsServerResourceRecordA -Name $NFSKerberosName +  -IPv4Address $SVMipAddress -ZoneName $configHashTable["DOMAIN"] -ComputerName $DCname 
 $reverseZone = "0.168.192.in-addr.arpa"
-$reverseRecordName = "130"
-Add-DnsServerResourceRecordPtr -Name $reverseRecordName -PtrDomainName "NFS-SVM1.demo.netapp.com" -ZoneName $reverseZone -ComputerName "DC1.demo.netapp.com"
+$reverseZone = $SVMipAddress.split('.')[2] + "." + $SVMipAddress.split('.')[1] + "." + $SVMipAddress.split('.')[0] + ".in-addr.arpa"
+$reverseRecordName = $SVMipAddress.split('.')[3] 
+Add-DnsServerResourceRecordPtr -Name $reverseRecordName -PtrDomainName $NFSKerberosName -ZoneName $reverseZone -ComputerName $DCname 
