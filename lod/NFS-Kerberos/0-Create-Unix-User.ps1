da78b35d04bea49f5f11a78af0217b17d20206df
unknown <Administrator@DEMO.NETAPP.COM>
2025-02-11 17:20:15 +0000

    Check AD user

--- lod/NFS-Kerberos/0-Create-Unix-User.ps1
+++ lod/NFS-Kerberos/0-Create-Unix-User.ps1
@@ -23,16 +23,40 @@ $configHashTable["DOMAIN"]
 $configHashTable["LINUX_HOSTNAME"]
 $configHashTable["LINUX_IP"]
 
-New-ADGroup -Name Unix -GroupScope DomainLocal -OtherAttributes @{'gidnumber'='10001'}
-Get-ADGroup Unix -Properties gidNumber
+try {
+    $groupObject = Get-ADGroup Unix -ErrorAction Stop 
+    Write-Output "Unix Group already exists on"
+} catch {
+   New-ADGroup -Name Unix -GroupScope DomainLocal -OtherAttributes @{'gidnumber'='10001'}
+   Get-ADGroup Unix -Properties gidNumber
+}
 
 $password = ConvertTo-SecureString "Netapp1!" -AsPlainText -Force
 
-New-ADUser -SamAccountName user1 -UserPrincipalName user1@DEMO.NETAPP.COM -Name user1 -AccountPassword $password -OtherAttributes @{'uid'="user1";'uidNumber'="10001";'gidNumber'="10001";'unixHomeDirectory'='/home/user1';'loginShell'='/bin/bash'} -Enabled 1 -PasswordNeverExpires 1
 
-New-ADUser -SamAccountName user2 -UserPrincipalName user2@DEMO.NETAPP.COM -Name user2 -AccountPassword $password -OtherAttributes @{'uid'="user2";'uidNumber'="10002";'gidNumber'="10001";'unixHomeDirectory'='/home/user2';'loginShell'='/bin/bash'} -Enabled 1 -PasswordNeverExpires 1
+try {
+    $userObject = Get-ADUser -Identity user1 -ErrorAction Stop
+    Write-Output "user1 already exists on"
+} catch {
+    Write-Output "user1 create"
+    New-ADUser -SamAccountName user1 -UserPrincipalName user1@DEMO.NETAPP.COM -Name user1 -AccountPassword $password -OtherAttributes @{'uid'="user1";'uidNumber'="10001";'gidNumber'="10001";'unixHomeDirectory'='/home/user1';'loginShell'='/bin/bash'} -Enabled 1 -PasswordNeverExpires 1
+}
 
-New-ADUser -SamAccountName user3 -UserPrincipalName user3@DEMO.NETAPP.COM -Name user3 -AccountPassword $password -OtherAttributes @{'uid'="user3";'uidNumber'="10003";'gidNumber'="10001";'unixHomeDirectory'='/home/user3';'loginShell'='/bin/bash'} -Enabled 1 -PasswordNeverExpires 1
+try {
+    $userObject = Get-ADUser -Identity user2 -ErrorAction Stop
+    Write-Output "user2 already exists on"
+} catch {
+    Write-Output "user2 create"
+    New-ADUser -SamAccountName user2 -UserPrincipalName user2@DEMO.NETAPP.COM -Name user2 -AccountPassword $password -OtherAttributes @{'uid'="user2";'uidNumber'="10002";'gidNumber'="10001";'unixHomeDirectory'='/home/user2';'loginShell'='/bin/bash'} -Enabled 1 -PasswordNeverExpires 1
+}
+
+try {
+    $userObject = Get-ADUser -Identity user3 -ErrorAction Stop
+    Write-Output "user3 already exists on"
+} catch {
+    Write-Output "user3 create"
+    New-ADUser -SamAccountName user3 -UserPrincipalName user3@DEMO.NETAPP.COM -Name user3 -AccountPassword $password -OtherAttributes @{'uid'="user3";'uidNumber'="10003";'gidNumber'="10001";'unixHomeDirectory'='/home/user3';'loginShell'='/bin/bash'} -Enabled 1 -PasswordNeverExpires 1
+}
 
 # Add DNS Entry for Kerberos SVM IP
 $NFSKerberosName="NFS-" + $configHashTable["SVM1"]
