04a38ef8dbf65fa1090ea46b3a71eebdf76282f6
jbnetapp <jerome.blanchet@netapp.com>
2025-02-11 16:17:38 +0100

    commit

--- lod/NFS-Kerberos/0-Create-Unix-User.ps1
+++ lod/NFS-Kerberos/0-Create-Unix-User.ps1
@@ -49,7 +49,6 @@ Add-DnsServerResourceRecordPtr -Name $reverseRecordName -PtrDomainName $NFSKerbe
 Add-DnsServerResourceRecordA -Name $configHashTable["LINUX_HOSTNAME"] -IPv4Address $configHashTable["LINUX_IP"] -ZoneName $configHashTable["DOMAIN"] -ComputerName $DCname
 $reverseZone = $configHashTable["LINUX_IP"].split('.')[2] + "." + $configHashTable["LINUX_IP"].split('.')[1] + "." + $configHashTable["LINUX_IP"].split('.')[0] + ".in-addr.arpa"
 $reverseRecordName = $configHashTable["LINUX_IP"].split('.')[3]
-
 $LinuxNameFQDN=$configHashTable["LINUX_HOSTNAME"] + "." + $configHashTable["DOMAIN"]
 
 Add-DnsServerResourceRecordPtr -Name $reverseRecordName -PtrDomainName $LinuxNameFQDN -ZoneName $reverseZone -ComputerName $DCname
